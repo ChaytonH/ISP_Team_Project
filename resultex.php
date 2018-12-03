3eb81384c08d186758d62e996d08b85987e32975
ChaytonH <cnh47@zips.uakron.edu>
2018-12-03 18:24:22 -0500

    Reuploaded whole project
    Re-uploaded whole project from my pausch

--- resultex.php
+++ resultex.php
@@ -9,11 +9,13 @@
   <body style =" background-color:lightgrey;">
   	<div style = " z-index : 1; text-align:center; background-color:white; position: -webkit-sticky; position: sticky; top: 0; ">
 		<img width = "100" src = "Page Essentials\logo.jpg"></img>
-		<input size = "50" name = "searchbar" />
-		<button onclick="">Search</button>
+        <form method="get" action="resultex.php">
+            <input size = "50" name = "searchbar" />
+    		<input type="submit" > <!-- Make this point to a function that takes the input and finds it from our database -->
+        </form>
 		<a href = "instructions.html">Online Help</a><!-- Make this point to a function that takes the input and finds it from our database -->
 	</div>
-	<p> Make this a table like the homepage, but showing the results</p>
+	
 
 	  		<div style = "text-align:center"><button onclick="window.location.href='video storage.php'" >Return to Homepage</button></div>
             <br />
@@ -24,28 +26,34 @@
                 <th>Description</th>
             </tr>
             <?php
-                $UserInput = "%" . $_GET["searchbar"] . "%";
-                $db = mysqli_connect("db1.cs.uakron.edu:3306", "dlc190", "passwordlol");
-                $qu = "SELECT * FROM Videos WHERE Name LIKE " . $UserInput;
-                // print $qu;
-                $result = mysqli_query($db, $qu);
 
-                echo "<table border='1' style='width:100%'>";
+                    $UserInputs = "'%".$_GET["searchbar"]."%'";
 
-                $row = mysqli_fetch_array($result);
-                if ($row) {
-                    while($row)
+                    $db = mysqli_connect("db1.cs.uakron.edu:3306", "dlc190", "passwordlol");
+
+                    if (!$db)
+                    {
+                        print "Error - Could not connect to MySQL";
+                        exit;
+                    }
+
+                    $er = mysqli_select_db($db,"ISP_dlc190");
+                    if (!$er)
+                    {
+                        print "Error - Could not select the database";
+                        exit;
+                    }
+
+                    $qu = "SELECT * FROM Videos Where Name Like ".$UserInputs;
+                    $result = mysqli_query($db, $qu);
+
+                    echo "<table border='1' style='width:100%'>";
+
+                    while($row = mysqli_fetch_array($result))
                     {
                         echo "<tr><td><video width ='320' height ='200' controls='controls'><source src='upload/".$row['Name']."'> Your browser does not support the video element</audio></td><td>".$row['Name']."</td><td>".$row['Descript']."</td></tr>";
                     }
                     echo "</table>";
-                }
-                else {
-                    print "No videos found";
-                }
-
-
-
             ?>
 
   </body>
--- submission.php
+++ submission.php
@@ -1,17 +1,17 @@
 <?php
 
 if(isset($_POST['Upload']))
-{	
+{
 	$db = mysqli_connect("db1.cs.uakron.edu:3306", "dlc190", "passwordlol");
 
-	if (!$db) 
+	if (!$db)
 	{
      		print "Error - Could not connect to MySQL";
      		exit;
 	}
 
 	$er = mysqli_select_db($db,"ISP_dlc190");
-	if (!$er) 
+	if (!$er)
 	{
     		print "Error - Could not select the database";
     		exit;
@@ -21,8 +21,8 @@ if(isset($_POST['Upload']))
 	$descript = $_POST['description'];
 	$target = "upload/";
 	$target = $target . basename($_FILES['video']['name']);
-	
-	move_uploaded_file($_FILES['video']['tmp_name'], $target); 
+
+	move_uploaded_file($_FILES['video']['tmp_name'], $target);
 
 	$qu = "INSERT INTO Videos values('$video', '$descript')";
 
@@ -50,27 +50,29 @@ if(isset($_POST['Upload']))
   <body style =" background-color:lightgrey;">
   	<div style = " z-index : 1; text-align:center; background-color:white; position: -webkit-sticky; position: sticky; top: 0; ">
 		<img width = "100" src = "Page Essentials\logo.jpg"></img>
-		<input size = "50" id = "search bar" />
-		<button onclick="">Search</button> 
+		<form method="get" action="resultex.php">
+            <input size = "50" name = "searchbar" />
+    		<input type="submit" > <!-- Make this point to a function that takes the input and finds it from our database -->
+        </form>
 		<a href = "instructions.html">Online Help</a><!-- Make this point to a function that takes the input and finds it from our database -->
 	</div>
-	
+
 	<h2 style = "text-align:center"> Upload A Video </h2>
-		
+
 	<h3> File </h3>
-	
+
 	<form action="submission.php" method="post" enctype="multipart/form-data">
-			
+
 		<input type="file" name="video" /><br/>
-		
+
 		<h3> Description </h3>
-		
+
 		<input name="description" /><br/><br/>
-	
+
 	<input type="submit" name="Upload" id="Upload" value="Upload" />
-	
+
 	</form>
-	
-		<div style = "text-align:center"><button onclick="window.location.href='video storage.php'" >Return to Homepage</button></div>	
+
+		<div style = "text-align:center"><button onclick="window.location.href='video storage.php'" >Return to Homepage</button></div>
   </body>
 </html>
